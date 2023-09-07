3d0808a5fa55b90bf8aae9fd542960688f3af24d
Reese Chong <dev.reese.chong@gmail.com>
2023-09-07 13:22:10 -0400

    Add code from Thu, 07 Sep 2023 13:22:10 -0400

---
+++ 15-Ex2JavaScanner-ReeseChong/Library.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Library {
+  public static Scanner input = new Scanner(System.in);
+
+  // ***** Text Colors for the Console *****************//
+  public static final String TEXT_RESET = "\u001B[0m";
+  public static final String TEXT_BLACK = "\u001B[30m";
+  public static final String TEXT_RED = "\u001B[31m";
+  public static final String TEXT_GREEN = "\u001B[32m";
+  public static final String TEXT_YELLOW = "\u001B[33m";
+  public static final String TEXT_BLUE = "\u001B[34m";
+  public static final String TEXT_PURPLE = "\u001B[35m";
+  public static final String TEXT_CYAN = "\u001B[36m";
+  
+} // bracket belongs to the class
