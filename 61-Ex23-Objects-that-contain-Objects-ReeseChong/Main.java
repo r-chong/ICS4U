bb594fb03082dd398b66ae1350628a2e81e579f3
Reese Chong <dev.reese.chong@gmail.com>
2023-12-05 11:51:31 -0500

    Add code from Tue, 05 Dec 2023 11:51:31 -0500

---
+++ 61-Ex23-Objects-that-contain-Objects-ReeseChong/Main.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String[] args) {
+        ConeTester.run();
+        CylinderTester.run();
+        TreeTester.run();
+        ForestTester.run();
+    }
+}
