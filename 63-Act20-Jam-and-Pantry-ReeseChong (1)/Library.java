a6ed77e4d53e553af41677d04946c9874e4059f1
Reese Chong <dev.reese.chong@gmail.com>
2023-12-06 12:04:01 -0500

    Add code from Wed, 06 Dec 2023 12:04:01 -0500

---
+++ 63-Act20-Jam-and-Pantry-ReeseChong (1)/Library.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.text.DecimalFormat;
+
+class Library {
+
+    public static Scanner input = new Scanner(System.in);
+
+    public static DecimalFormat dollarFormat = new DecimalFormat("$###,###.##");
+
+    // ***** Text Colors for the Console *****************//
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_BLACK = "\u001B[30m";
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_YELLOW = "\u001B[33m";
+    public static final String TEXT_BLUE = "\u001B[34m";
+    public static final String TEXT_PURPLE = "\u001B[35m";
+    public static final String TEXT_CYAN = "\u001B[36m";
+    public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
+    public static final String p = "\033[1;92m"; // GREEN
+
+    public static int myRandom(int min, int max) {
+        // up to 1 less than max
+        int temp;
+        temp = (int) (Math.random() * (max - min) + min);
+
+        return temp;
+    }// myRandom
+
+    public static int stringSearch(String[] arr, String searchTerm) {
+        for (int i = 0; i < arr.length; i++) {
+            if (searchTerm.equalsIgnoreCase(arr[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }// stringSearch
+
+    public static int linearStringSearch(ArrayList<String> list, String searchTerm) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equalsIgnoreCase(searchTerm)) {
+                return i;
+            }
+        }
+        return -1;
+    }// linearStringSearch
+
+}// class
