1fe54473b775277e64c6eddac8df63ee44b74965
Reese Chong <dev.reese.chong@gmail.com>
2023-12-09 01:43:27 -0500

    Add code from Sat, 09 Dec 2023 01:43:27 -0500

---
+++ 63-Act20-Jam-and-Pantry-ReeseChong (1)/ExpirationTester.java
@@ -0,0 +1,9 @@
+public class ExpirationTester {
+    public static void run() {
+        ExpirationDate date1 = new ExpirationDate(05, 12, 2023, 100);
+        ExpirationDate date2 = new ExpirationDate("01122023", 100);
+
+        System.out.println(date1);
+        System.out.println(date2);
+    }
+}
