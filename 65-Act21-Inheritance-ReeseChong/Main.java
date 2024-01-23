3863a4cbc39979646edd6e95d626040aacd5f388
Reese Chong <dev.reese.chong@gmail.com>
2024-01-22 19:21:14 -0500

    Add code from Mon, 22 Jan 2024 19:21:14 -0500

---
+++ 65-Act21-Inheritance-ReeseChong/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        // PokemonTester.run();
+        // ElectricTypeTester.run();
+        // RockTypeTester.run();
+        PokeBallTester.run();
+    }
+}
