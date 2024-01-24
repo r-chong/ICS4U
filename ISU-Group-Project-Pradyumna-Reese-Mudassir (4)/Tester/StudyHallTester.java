61010a39558bd9233343b35c4daa22b7155d8f7d
Reese Chong <dev.reese.chong@gmail.com>
2024-01-23 22:23:02 -0500

    Add code from Tue, 23 Jan 2024 22:23:02 -0500

---
+++ ISU-Group-Project-Pradyumna-Reese-Mudassir (4)/Tester/StudyHallTester.java
@@ -0,0 +1,20 @@
+/*
+2024-01-17
+Tester class for StudyHall
+*/
+
+import Campus.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StudyHallTester {
+    public static void main(String[] args) {
+
+      //Creating a Studyhall Object 
+      StudyHall studyHall1 = new StudyHall("Mah Building", 200, 2, new ArrayList<String>(Arrays.asList("Ergonomic Seating","Chalkboard")),true,4000.0,6);
+    System.out.println(studyHall1);
+    System.out.println(studyHall1);
+    System.out.println(studyHall1);
+}
+}//end class
