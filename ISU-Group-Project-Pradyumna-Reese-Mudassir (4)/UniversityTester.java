ff7c29d8db4911a4387cad9fdae811d0252fe6bc
Reese Chong <dev.reese.chong@gmail.com>
2024-01-23 23:01:55 -0500

    Add code from Tue, 23 Jan 2024 23:01:55 -0500

---
+++ ISU-Group-Project-Pradyumna-Reese-Mudassir (4)/UniversityTester.java
@@ -0,0 +1,19 @@
+/*
+2024-01-17
+Tester class for University
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UniversityTester {
+
+    public static void main(String[] args) {
+        // obj name doesn't really matter as they only see their own name var :)
+        University bucklandU = new University("Buckland University", 1, 100000.0);
+        
+        // Test case 1: constructor
+        System.out.println(bucklandU);
+        
+    }
+}//end class
