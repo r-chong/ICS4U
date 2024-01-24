9cf0cfe88be7841ba3f2468e62e2141200251517
Reese Chong <dev.reese.chong@gmail.com>
2024-01-23 23:01:55 -0500

    Add code from Tue, 23 Jan 2024 23:01:55 -0500

---
+++ ISU-Group-Project-Pradyumna-Reese-Mudassir (4)/Tester/ResidenceTester.java
@@ -0,0 +1,20 @@
+/*
+2024-01-17
+Tester class for Residence
+*/
+
+import Campus.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ResidenceTester {
+    public static void main(String[] args) {
+
+      //Creating a Residence object 
+        Residence residence1 = new Residence("Shyamilan Commons", 200, 2, new ArrayList<String>(Arrays.asList("Gorilla Glass Windows","Beds","Kitchens")),4000.0,true,40,120);
+    System.out.println(residence1);
+    System.out.println(residence1);
+    System.out.println(residence1);
+}
+}//end class
