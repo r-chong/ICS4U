79d4e47f0c3e4c08b8048665e639d4496b9dff21
Reese Chong <dev.reese.chong@gmail.com>
2024-01-23 23:04:00 -0500

    Add code from Tue, 23 Jan 2024 23:04:00 -0500

---
+++ ISU-Group-Project-Pradyumna-Reese-Mudassir (4)/Constants/LocationData.java
@@ -0,0 +1,11 @@
+//constants file
+
+package Constants;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LocationData {
+    public static final ArrayList<String> LOCATION_NAMES = new ArrayList<String>(Arrays.asList("Ontario","Quebec","BC","Nunavut","The Moon"));
+
+    public static final ArrayList<Integer> LOCATION_DETAILS = new ArrayList<Integer>(Arrays.asList(14500000,8500000,5000000,36000,0));
+}
